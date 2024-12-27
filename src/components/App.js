813901914de937ebe8a33491d2dd1b3dd5143645
vaibha987 <v2goyal95@gmail.com>
2024-12-27 15:48:37 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,16 @@
 import React, { useState, useEffect } from "react";
-import WeatherDisplay from "./weatherDisplay";
+import WeatherDisplay from "./WeatherDisplay";
 
 const App = () => {
-  // State to store weather data
   const [weather, setWeather] = useState({ temperature: 0, conditions: "" });
 
   useEffect(() => {
-    // Simulating fetching weather data
     const weatherInput = { temperature: 25, conditions: "Sunny" };
     setWeather(weatherInput);
   }, []);
 
   return (
-    <div style={{ fontFamily: "Arial, sans-serif", textAlign: "center", marginTop: "50px" }}>
+    <div style={{ textAlign: "center", fontFamily: "Arial, sans-serif", marginTop: "50px" }}>
       <h1>Weather Application</h1>
       <WeatherDisplay weather={weather} />
     </div>
